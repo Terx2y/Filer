980f6734c712ee05dadd78e05adf49a3ca269db4
Terx2y <r4n3@mail.ru>
2018-08-05 01:19:41 +0800

    Filer class realization (not ready yet)

---
+++ core/Filer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+#include <cstdlib>
+#include <map>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+
+#include "Filer.hpp"
+
+bool Filer::addFolder(char* dirName, char* langType, char* projectType)
+{
+    
+}
