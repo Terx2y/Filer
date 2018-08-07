a8155057ad2bf60961b39273161e13070777c7aa
Лежанин Иван Иванович <C:\Users\avr\AppData\Roaming\The Bat!>
2018-08-07 17:49:37 +0800

    Deleted std namespace

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include "core/Filer.hpp"
 
-using namespace std;
-
 int main(int argc, char* argv[], char* env[])
 {
     // Create Filer class copy
@@ -10,7 +8,7 @@ int main(int argc, char* argv[], char* env[])
 
     if(argv[1] == NULL)
     {
-        cout << "Argument can not be empty!\n";
+        std::cout << "Argument can not be empty!\n";
     }
     else{
         map <string, int> language; // Type of a language in project that we init
@@ -18,29 +16,29 @@ int main(int argc, char* argv[], char* env[])
         language["-c"] = 1; // C Language
         language["-php"] = 2; // PHP language
         language["-cpp"] = 3; // C++ language
-        language["-html"] = 4; // HTML markdown
+        language["-html"] = 4; // HTML markup
 
         if(argv[2] == NULL)
         {
-            cout << "Error.. \n Project type not mentioned!\n\nSyntax: filer FolderName -language type\n\ntypes: -html\t-c\t-cpp\t-php\n";
+            std::cout << "Error.. \n Project type not mentioned!\n\nSyntax: filer FolderName -language type\n\ntypes: -html\t-c\t-cpp\t-php\n";
             exit(-1);
         }
 
         // Here we moved index position for 1 step to the right, to truncate " - " before extention
         // langType = language type (html, c, c++, php)
-        char* langType = argv[2] + 1; 
+        char* langType = argv[2] + 1;
         
         switch(language[argv[2]])
         {            
             case 1:                
                 if(project.addFolder(argv[1], langType, 'cLangs'))
                 {
-                    cout << "Project: " << argv[1] << " added successful\n";
+                    std::cout << "Project: " << argv[1] << " added successful\n";
                 }
             break;
 
             default:
-                cout << "Unknown argument:  " << argv[2] << "\n";
+                std::cout << "Unknown argument:  " << argv[2] << "\n";
             break;
         }    
     
