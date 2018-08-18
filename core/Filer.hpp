aabcd1e24a233ff834625f366df8998ad1a70113
Terx2y <r4n3@mail.ru>
2018-08-19 01:26:47 +0800

    Improved class description

--- core/Filer.hpp
+++ core/Filer.hpp
@@ -1,17 +1,18 @@
 #ifndef _FILER_H
 #define _FILER_H
-
-    #include "dirent.h"  // mkdir and chdir here ..
+    #include <string>
 
     /*
-    **  dirName = название директории; 
-    **  langType = тип (или название) языка (-html; -php; -c; -cpp)
-    **  projectType = тип проекта. В зависимости от типа проекта будет создаваться определённая структура файлов (web; cLangs)
+        **  Filer class creates our work directory
+        **
+        **  dirName = название директории;
+        **  langType = тип (или название) языка (-html; -php; -c; -cpp)
+        **  projectType = тип проекта. В зависимости от типа проекта будет создаваться определённая структура файлов (web; cLangs)
     */
     class Filer
     {
-        public: 
-            bool addFolder(char* dirName, char* langType, char* projectType);
+        public:
+            bool addFolder(char* dirName, char* langType, std::string projectType);
     };
 
 #endif
